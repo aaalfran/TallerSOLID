529c85e239e625248cdae090f02b471742d1ebbd
hvillega99 <hvillega@espol.edu.ec>
2020-06-25 13:38:31 -0500

    método main funcional

--- TallerSOLID/src/Principal/Sistema.java
+++ TallerSOLID/src/Principal/Sistema.java
@@ -4,45 +4,43 @@
  * and open the template in the editor.
  */
 package Principal;
-import Adicionales.Aderezo;
 import Adicionales.CREMA;
 import Adicionales.FRUTILLA;
 import Postres.*;
 import Procesos.*;
 import Leche.*;
+import java.util.ArrayList;
 /**
  *
  * @author djurado
  */
 public class Sistema {
-    public static void main(String [ ] args){
+
+    
+        public static void main(String [ ] args){
         // Producir un helado de vainilla y una torta de chocolate, 
         // a ambos agregarles CREMA y FRUTILLAS
-        // y cambiar el tipo de leche por Leche Descremada
-        // Finalmente mostrar el precio final de cada uno
-        LecheEntera leche = new LecheDeslactosada();
-        ManejadorDeLeche mnj_leche = new ManejadorDeLeche();
+        // y cambiar el tipo de leche por Leche Deslactosada
+        ArrayList<Postre> arrPostres = new ArrayList<>();
+        ManejadorDeLeche mnj_leche = new ManejadorDeLeche(new LecheDescremada());
         
         // Producir Helado
-        Helado helado_vainilla = new Helado("Vainilla");
-        
-        CREMA C = new CREMA();
-        FRUTILLA F = new FRUTILLA();
-        
-        helado_vainilla.anadirAderezo(C);
-        helado_vainilla.anadirAderezo(F);
-        System.out.println(helado_vainilla);
-        mnj_leche.cambiarTipoLeche(leche, helado_vainilla); //1
-        System.out.println(helado_vainilla.showPrecioFinal());
-        
+        Postre helado_vainilla = new Helado("Vainilla");
+        arrPostres.add(helado_vainilla);
         // Producir Pastel
-        Pastel pastel_chocolate = new Pastel("Chocolate");
-        pastel_chocolate.quitarAderezo(C);
-        pastel_chocolate.anadirAderezo(F);
-        System.out.println(pastel_chocolate);
-        mnj_leche.cambiarTipoLeche(leche, pastel_chocolate); //1
-        System.out.println(helado_vainilla.showPrecioFinal());
+        Postre pastel_chocolate = new Pastel("Chocolate");
+        arrPostres.add(pastel_chocolate);
         
+        arrPostres.forEach(postre -> {
+            postre.anadirAderezo(new CREMA());
+            postre.anadirAderezo(new FRUTILLA());
+            System.out.println(postre);
+            LecheEntera leche = new LecheDeslactosada();
+            mnj_leche.cambiarTipoLeche(leche,postre);
+            System.out.println(ManejadorDePrecio.showPrecioFinal(postre));
+        });        
         
     }
+
+
 }
